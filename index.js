f6a8d3c89accd2c27029bf14ed127d0267888ca9
DarnishaTurner <darnishaturner@pursuit.org>
2023-12-16 09:59:36 -0500

    Darnisha finished work

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-let startNum = 5; // Change this number to test with different values
+let startNum = 8; // Change this number to test with different values
 
 // TODO: Use a while loop for the reverse countdown
 
+while (startNum >= 0) {
+    console.log(startNum);
+    startNum --; //This line decrements startNum by 1 in each iteration.
+}
